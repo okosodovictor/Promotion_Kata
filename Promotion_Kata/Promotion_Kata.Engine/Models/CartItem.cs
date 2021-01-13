95c180c10483d8249e7301049d404aeca4b6c7d3
Victor <okosodovictor@yahoo.com>
2021-01-13 22:59:02 +0100

    Added CartItem class

---
+++ Promotion_Kata/Promotion_Kata.Engine/Models/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Promotion_Kata.Engine.Models
+{
+    public class CartItem
+    {
+		public Product Product { get; }
+		public int Count { get; }
+		public CartItem(Product product, int count)
+		{
+			Product = product;
+			Count = count;
+		}
+	}
+}
